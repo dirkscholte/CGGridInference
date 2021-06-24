0a2b9a1bd789b7920776f879d521fe05cdc72628
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-06-24 15:27:06 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="CGGridInference",
+    version="0.1",
+    author="Dirk Scholte",
+    author_email="dirk.scholte.20@ucl.ac.uk",
+    description="Python package to fit data to modelled emission lines.",
+    packages=["CGGridInference", "numpy"]
+)
