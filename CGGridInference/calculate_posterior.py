63caf567dbd0c0467b96d658d3289ecf25c519fd
Dirk Scholte <dirk.scholte.20@ucl.ac.uk>
2021-06-30 14:51:45 +0100

    Rewrote calculate_posterior.py using numpy masking.

--- CGGridInference/calculate_posterior.py
+++ CGGridInference/calculate_posterior.py
@@ -6,17 +6,20 @@ class CalculatePosterior:
     def __init__(self, model_flux_values, model_line_labels):
         self.model_flux_values = model_flux_values
         self.model_line_labels = dict(zip(model_line_labels, range(len(model_line_labels))))
-        self.data_flux_values = np.array([])
-        self.data_flux_errors = np.array([])
-        self.data_line_labels = dict()
+        self.signal_to_noise_limit = 3.0
+        self.detection_flux_values = np.ma.masked_invalid(np.ones((len(self.model_line_labels)))*np.nan)
+        self.detection_flux_errors = np.ma.masked_invalid(np.ones((len(self.model_line_labels)))*np.nan)
+        self.uplim_flux_values = np.ma.masked_invalid(np.ones((len(self.model_line_labels)))*np.nan)
+        self.uplim_flux_errors = np.ma.masked_invalid(np.ones((len(self.model_line_labels)))*np.nan)
         self.prior = np.ones(self.model_flux_values.shape[-1]) / np.sum(np.ones(self.model_flux_values.shape[-1]))
         self.likelihood = np.ones(self.model_flux_values.shape[-1]) * np.nan
         self.posterior = np.ones(self.model_flux_values.shape[-1]) * np.nan
-        self.signal_to_noise_limit = 3.0
 
     def reset_likelihood(self):
-        self.data_flux_values = np.array([])
-        self.data_line_labels = np.array([])
+        self.detection_flux_values = np.ma.masked_invalid(np.ones((len(self.model_line_labels))) * np.nan)
+        self.detection_flux_errors = np.ma.masked_invalid(np.ones((len(self.model_line_labels))) * np.nan)
+        self.uplim_flux_values = np.ma.masked_invalid(np.ones((len(self.model_line_labels))) * np.nan)
+        self.uplim_flux_errors = np.ma.masked_invalid(np.ones((len(self.model_line_labels))) * np.nan)
         self.likelihood = np.ones(self.model_flux_values.shape[-1]) * np.nan
         self.posterior = np.ones(self.model_flux_values.shape[-1]) * np.nan
 
@@ -26,47 +29,46 @@ class CalculatePosterior:
         return dim_array
 
     def input_data(self, data_flux_values, data_flux_errors, data_line_labels):
-        self.data_line_labels = dict(zip(data_line_labels, range(len(data_line_labels))))
-        self.data_flux_values = data_flux_values
-        self.data_flux_errors = data_flux_errors
+        for i in range(len(data_line_labels)):
+            idx = self.model_line_labels.get(data_line_labels[i])
+            if data_flux_values[i]/data_flux_errors[i] >= self.signal_to_noise_limit:
+                self.detection_flux_values[idx] = data_flux_values[i]
+                self.detection_flux_errors[idx] = data_flux_errors[i]
+            else:
+                self.uplim_flux_values[idx] = self.signal_to_noise_limit * data_flux_errors[i]           #F. Masci 10/25/2011 Computing flux upper-limits for non-detections
+                self.uplim_flux_errors[idx] = (self.signal_to_noise_limit + 2.054) * data_flux_errors[i]
 
 
     def normalize_model(self, line_label):
         self.model_flux_values = self.model_flux_values / np.take(self.model_flux_values, self.model_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.model_flux_values.ndim, range(self.model_flux_values.ndim-1)))
 
     def normalize_data(self, line_label):
-        self.data_flux_errors = self.data_flux_errors / np.take(self.data_flux_values, self.data_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.data_flux_values.ndim, range(self.data_flux_values.ndim-1)))
-        self.data_flux_values = self.data_flux_values / np.take(self.data_flux_values, self.data_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.data_flux_values.ndim, range(self.data_flux_values.ndim-1)))
+        self.uplim_flux_errors = self.uplim_flux_errors / np.take(self.detection_flux_values, self.model_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.detection_flux_values.ndim, range(self.detection_flux_values.ndim - 1)))
+        self.uplim_flux_values = self.uplim_flux_values / np.take(self.detection_flux_values, self.model_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.detection_flux_values.ndim, range(self.detection_flux_values.ndim - 1)))
+        self.detection_flux_errors = self.detection_flux_errors / np.take(self.detection_flux_values, self.model_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.detection_flux_values.ndim, range(self.detection_flux_values.ndim-1)))
+        self.detection_flux_values = self.detection_flux_values / np.take(self.detection_flux_values, self.model_line_labels.get(line_label), axis=-1).reshape(self.reshape_dims(self.detection_flux_values.ndim, range(self.detection_flux_values.ndim-1)))
 
-    def mask_unused_lines(self):
-        line_indices = np.array([self.model_line_labels.get(key) for key in list(self.data_line_labels.keys())], dtype=int)
-        mask_lines = np.array([np.any(value == line_indices) for value in list(self.model_line_labels.values())], dtype=int)
-        mask_lines_model = np.ones(self.model_flux_values.shape, dtype=bool) * mask_lines.reshape(self.reshape_dims(self.model_flux_values.ndim, -1))
-        return mask_lines, mask_lines_model
-
-    def mask_uplims(self):
-        mask_uplims_data = (self.data_flux_values >= self.signal_to_noise_limit * self.data_flux_errors)
-        mask_uplims_model = np.ones(self.model_flux_values.shape[-1], dtype=bool) * mask_uplims_data.reshape(self.reshape_dims(self.model_flux_values.ndim, -1))
-        return mask_uplims_data, mask_uplims_model
-
-    def calculate_lnlikelihood(self):
+    def calculate_likelihood(self):
         def calc_lnlikelihood_detections(model, data, data_errors):
             return -0.5 * np.sum( ((data - model) / data_errors)**2, axis=-1)
         def calc_lnlikelihood_uplims(model, data, data_errors):
-            return np.sum( -np.log(2) + np.log( erf( (data - model) / data_errors) ) )
+            return np.sum( -np.log(2) + np.log( erf( (data - model) / data_errors) ), axis=-1)
 
-        mask_lines, mask_lines_model = self.mask_unused_lines()
-        mask_uplims_data, mask_uplims_model = self.mask_uplims()
-        lnlikelihood_detections = calc_lnlikelihood_detections(self.model_flux_values[mask_lines_model][mask_uplims_model],
-                                                               self.data_flux_values[mask_uplims_data].reshape(self.reshape_dims(self.model_flux_values.ndim, -1)),
-                                                               self.data_flux_errors[mask_uplims_data].reshape(self.reshape_dims(self.model_flux_values.ndim, -1)) )
-        lnlikelihood_uplims = calc_lnlikelihood_uplims(self.model_flux_values[mask_lines_model][~mask_uplims_model],
-                                                       self.data_flux_values[~mask_uplims_data].reshape(self.reshape_dims(self.model_flux_values.ndim, -1)),
-                                                       self.data_flux_errors[~mask_uplims_data].reshape(self.reshape_dims(self.model_flux_values.ndim, -1)) )
-        lnlikelihood = lnlikelihood_detections + lnlikelihood_uplims
-        return lnlikelihood
+        lnlikelihood_detections = calc_lnlikelihood_detections(self.model_flux_values,
+                                                               self.detection_flux_values,
+                                                               self.detection_flux_errors)
+        lnlikelihood_uplims = calc_lnlikelihood_uplims(self.model_flux_values,
+                                                       self.uplim_flux_values,
+                                                       self.uplim_flux_errors)
+        if np.sum(~self.uplim_flux_values.mask)>0:
+            lnlikelihood = lnlikelihood_detections + lnlikelihood_uplims
+        else:
+            lnlikelihood = lnlikelihood_detections
+        self.likelihood = np.exp(lnlikelihood)
+        self.likelihood_detections = np.exp(lnlikelihood_detections)
+        self.likelihood_uplims = np.exp(lnlikelihood_uplims)
+        return self.likelihood
 
     def calculate_posterior(self):
-        self.likelihood = np.exp(self.calculate_lnlikelihood())
         self.posterior = self.prior*self.likelihood
         return self.posterior
